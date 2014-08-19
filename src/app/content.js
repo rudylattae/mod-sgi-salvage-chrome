d8a97013de3598e24885b9d32c8894a4bc1de9a5
Rudy Lattae <rudylattae@gmail.com>
2014-08-19 00:15:40 -0600

    Use Ractive for rendering bid items in scan view

--- src/app/content.js
+++ src/app/content.js
@@ -6,32 +6,63 @@
     TableRowIterator = core.TableRowIterator;
 
 
-  // perform global initialization
-  function init() {
-    //var itemsTable = $('#bid_items').length > 0 ? $('#bid_items') : $('#bid_results'),
-    var  appContainer = $('body'),
-      originalTableViewContainer = $('#salvageMainContent'),
-      scanViewElement = $('<div class="mod--scan-view"></div>').prependTo(appContainer),
-      mainTabs = $('<ul class="mod--main-tabs"><li><a href="#scan">Scan</a></li><li><a href="#table">Table</a></li></ul>')
-        .prependTo(appContainer); 
+  var itemsTable = $('#bid_items').length > 0 ? $('#bid_items') : $('#bid_results'),
+    appContainer = $('body'),
+    originalTableViewContainer = $('#salvageMainContent'),
+    scanViewElement = $('<div class="mod--scan-view"></div>').prependTo( appContainer );
 
-    function setActiveView(viewName) {
-      if (!viewName || viewName === '#scan') {
-        originalTableViewContainer.hide();
-        scanViewElement.show();
-      } else {
-        originalTableViewContainer.show();
-        scanViewElement.hide();
-      }
+
+  // Tabs
+  function setActiveView(viewName) {
+    if ( !viewName || viewName === '#scan' ) {
+      originalTableViewContainer.hide();
+      scanViewElement.show();
+    } else {
+      originalTableViewContainer.show();
+      scanViewElement.hide();
     }
-
-    mainTabs.on('click', 'a', function() {
-      setActiveView($(this).attr('href'));
-    });
-    
-    var tt = new TableRowIterator($('#bid_items'));
-    console.log( tt.next() );
   }
 
+  function wireupTabs() {
+    var mainTabs = $('<ul class="mod--main-tabs"><li><a href="#scan">Scan</a></li><li><a href="#table">Table</a></li></ul>')
+          .prependTo( appContainer );
+
+    mainTabs.on('click', 'a', function() {
+      setActiveView( $(this).attr('href') );
+    });
+  }
+
+
+  // Scan View
+  function createScanView() {
+    var items = [],
+      template = '\
+        {{#items}}\
+          <div class="mod--scan-item">Item {{make}} {{model}} {{year}}</div>\
+        {{/items}}\
+      ',
+      tt = new TableRowIterator( itemsTable ),
+      vm;
+
+    while ( tt.hasNext() ) {
+      items.push( tt.next() );
+    }
+
+    vm = new Ractive({
+      el: scanViewElement,
+      template: template,
+      data: { items: items }
+    });
+  }
+
+
+  // Initialization
+  function init() {
+    wireupTabs();
+    createScanView();
+  }
+
+
+  // Crank it up!
   init();
 }());
