a549a21df81d5850406076bebbbea6141297f033
Rudy Lattae <rudylattae@gmail.com>
2014-08-18 22:57:46 -0600

    Remove all non-essentials

--- src/app/content.js
+++ src/app/content.js
@@ -2,45 +2,7 @@
   'use strict';
 
   // imports
-  var $ = jQuery,
-    tofu = function(a,c){return a.replace(/{ *([^} ]+) *}/g,function(b,a){b=c;a.replace(/[^.]+/g,function(a){b=b[a]});return b})},
-    nsls = function(e){"use strict";function t(e,t){return e+"-"+t}function r(r,n){if(!r||""===r)throw new Error("You must provide a non-empty namespace");n=n||{};var o=n.storageAdapter||e.localStorage,m={setItem:function(e,n){o.setItem(t(r,e),n)},getItem:function(e){return o.getItem(t(r,e))},removeItem:function(e){o.removeItem(t(r,e))}};return m.set=m.setItem,m.get=m.getItem,m.remove=m.removeItem,m}return r}(window);
-
-
-  var ls = nsls('mod-sgi-cars');      // namespaced localStorage wrapper
-  
-  
-  var db = {
-    _items: [],
-
-    init: function() {
-      if (this._items.length === 0) {
-        var data = JSON.parse( ls.get('items') );
-        this._items = data || [];
-      }
-    },
-
-    all: function() {
-        return this._items;
-    },
-
-    add: function ( item ){
-      this._items.unshift( item );
-      ls.set('items', JSON.stringify( this._items ));
-    },
-
-    has: function( item ) {
-      var i = 0,
-        z = this._items.length;
-      for (; i < z; i++) {
-        if (this._items[i].stockNumber === item.stockNumber) 
-          return true;
-      }
-      return false;
-    }
-  };
-  db.init();
-
+  var $ = jQuery;
 
 
   // perform global initialization
@@ -70,8 +32,4 @@
   }
 
   init();
-
-
-  var tt = new core.TableRowIterator($('#bid_items'));
-  console.log( tt.next() );
 }());
